6c01f03a5760b62abb6c7674b13ef4d0ef382a1f
rms <rmnsfx@yandex.ru>
2019-03-26 08:41:07 +0500

    make one file

--- c++/raw_sample_from_484/raw_sample_from_484/main.cpp
+++ c++/raw_sample_from_484/raw_sample_from_484/main.cpp
@@ -15,6 +15,11 @@
 #include<netdb.h>
 
 #include <mutex>
+#include <ctime>
+#include <chrono>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+
 
 
 struct pack_points_signals
@@ -66,47 +71,66 @@ int32_t yy = 0;
 int32_t zz = 0;
 bool ready_to_write = false;
 std::mutex g_lock;
+struct timeval tv;
+std::ofstream myfile;
+time_t curtime;
 
 
 
 
 
-void* write_to_file(void* args)
-{	
-	//std::vector<processed_data>* big_buffer_th  = (std::vector<processed_data>*) args;
-	
-	std::ofstream myfile;
-	myfile.open("example.txt", std::ios_base::app);
-
-	try
-	{
-		g_lock.lock();
-		
-
-		for (int i = 0; i < big_buffer.size(); i++)
-		{
-			myfile << big_buffer[i].x << " " << big_buffer[i].y << " " << big_buffer[i].z << std::endl;
-		}
-
-		ready_to_write = false;
 
 
-		g_lock.unlock();
-		
-
-		printf("write %d\n", big_buffer.size());
-		//printf("write %d\n");
-	}
-
-	catch (const std::exception&)
-	{
-		printf("write error\n");
-	}
-
-	myfile.close();
-
-	return 0;
-}
+//void* write_to_file(void* args)
+//{		
+//	char file_name[30];
+//	char buf[30];
+//
+//
+//	gettimeofday(&tv, NULL);
+//	curtime = tv.tv_sec;
+//	strftime(buf, 30, "%Y-%m-%d %T", localtime(&curtime));
+//		
+//	std::sprintf(file_name, "%s.csv", buf);
+//
+//
+//	//myfile.open(file_name, std::ios_base::app); //append to file
+//	myfile.open(file_name);
+//
+//
+//	try
+//	{
+//		g_lock.lock();
+//		
+//
+//		for (int i = 0; i < big_buffer.size(); i++)
+//		{
+//			gettimeofday(&tv, NULL);
+//			curtime = tv.tv_sec;
+//			strftime(buf, 30, "%Y-%m-%d %T.", localtime(&curtime));
+//
+//			myfile << buf << tv.tv_usec << ";" << big_buffer[i].x << ";" << big_buffer[i].y << ";" << big_buffer[i].z << std::endl;
+//		}
+//
+//		ready_to_write = false;
+//
+//
+//		g_lock.unlock();
+//		
+//
+//		printf("write %d\n", big_buffer.size());
+//		//printf("write %d\n");
+//	}
+//
+//	catch (const std::exception&)
+//	{
+//		printf("write error\n");
+//	}
+//
+//	myfile.close();
+//
+//	return 0;
+//}
 
 
 
@@ -114,13 +138,14 @@ int main()
 {
 	pthread_t write_thread;
 
-	timeout.tv_sec = 5;
+	timeout.tv_sec = 1;
 	timeout.tv_usec = 0;
 
 
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
+		
 		printf("\n Socket creation error \n");
 		//return -1;
 	}
@@ -128,6 +153,8 @@ int main()
 	{
 		printf("\n Socket create %d\n", sock);
 	}
+
+	
 
 	if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
 	{
@@ -139,7 +166,9 @@ int main()
 		printf("\n setsockopt failed \n");
 	}
 
+	//result = ioctl(sock, FIONBIO, 0); 
 
+	
 	serv_addr.sin_addr.s_addr = inet_addr(address);
 
 	serv_addr.sin_family = AF_INET;
@@ -154,69 +183,111 @@ int main()
 	}
 
 
-
+	char file_name[30];
+	char buf[30];
+	gettimeofday(&tv, NULL);
+	curtime = tv.tv_sec;
+	strftime(buf, 30, "%Y-%m-%d %T", localtime(&curtime));
+	std::sprintf(file_name, "%s.csv", buf);
+	//myfile.open(file_name, std::ios_base::app); //append to file
+	myfile.open(file_name);
+	fd_set set;
+	FD_ZERO(&set); /* clear the set */
+	FD_SET(sock, &set); /* add our file descriptor to the set */
 
 	while (1)
 	{
-			try
-			{
-				result = send(sock, buffer, sizeof(buffer), 0);
-				result = read(sock, &read_buffer, sizeof(read_buffer));
-			}
-			catch (const std::exception&)
-			{
-				printf("read dva error\n");
-			}
+
+			result = send(sock, buffer, sizeof(buffer), 0);
+			result = read(sock, &read_buffer, sizeof(read_buffer));
+			//result = select(sock + 1, &set, NULL, NULL, &timeout);
+
 		
-
-			pack_return_signals* pack = (pack_return_signals*) &read_buffer;
-
-			int count_point = pack->count_point > 1000 ? 0 : pack->count_point;
-
-			pack_points_signals* point = (pack_points_signals*) &pack->data;
-
-			while (count_point-- > 0)
+			if (result > 0)
 			{
+				
 
-					xx = (int32_t) ((point->data & 0x1FFFFF));
-					yy = (int32_t) ((point->data >> 21) & (0x1FFFFF));
-					zz = (int32_t) ((point->data >> 42) & (0x1FFFFF));
+				pack_return_signals* pack = (pack_return_signals*)&read_buffer;
 
-					//printf("%d -> %d \n", point->data, xx);
+				int count_point = pack->count_point > 1000 ? 0 : pack->count_point;
+
+				pack_points_signals* point = (pack_points_signals*)&pack->data;
+
+				while (count_point-- > 0)
+				{
+
+					xx = (int32_t)((point->data & 0x1FFFFF));
+					yy = (int32_t)((point->data >> 21) & (0x1FFFFF));
+					zz = (int32_t)((point->data >> 42) & (0x1FFFFF));
+
+
 
 					xx = xx > 0xFFFFF ? xx - 0x200000 : xx;
 					yy = yy > 0xFFFFF ? yy - 0x200000 : yy;
 					zz = zz > 0xFFFFF ? zz - 0x200000 : zz;
-										
-					
 
-					ready_data.x = xx;
-					ready_data.y = yy;
-					ready_data.z = zz;
 
-					point++;					
 
-					g_lock.lock();
+					//ready_data.x = xx;
+					//ready_data.y = yy;
+					//ready_data.z = zz;
 
-					if (big_buffer.size() > 5000) big_buffer.erase(big_buffer.begin());
-					big_buffer.push_back(ready_data);
+					point++;
 
-					g_lock.unlock();
+					//printf("%d %d %d\n", xx, yy, zz);
 
-					if (zz > 3000) ready_to_write = true;
+
+					//g_lock.lock();
+					//if (big_buffer.size() > 5000) big_buffer.erase(big_buffer.begin());
+					//big_buffer.push_back(ready_data);				
+					//if (count_point == 0) ready_to_write = true;
+
+
+
+					gettimeofday(&tv, NULL);
+					curtime = tv.tv_sec;
+					strftime(buf, 30, "%Y-%m-%d %T.", localtime(&curtime));
+
+					myfile << buf << tv.tv_usec << ";" << xx << ";" << yy << ";" << zz << std::endl;
+
+
+
+
+
+					//g_lock.unlock();
+
+
+					//printf("write %d\n", big_buffer.size());
+
+
+
+
+
+
+				//g_lock.unlock();
+
+				}
+
+				//if (ready_to_write == true)
+				//{
+				//	pthread_create(&write_thread, NULL, write_to_file, &big_buffer);
+				//}
+
+
+
+
+
+				printf("result %d\n", result);
+
+
+
+
+
+				//printf("%d\n", big_buffer.size());
+
+
+				//usleep(10000);					
 			}
-
-			if (ready_to_write == true)
-			{
-				pthread_create(&write_thread, NULL, write_to_file, &big_buffer);
-			}
-
-			//printf("%d %d %d\n", xx, yy, zz);
-			
-			//printf("%d\n", big_buffer.size());
-			
-
-			usleep(100000);					
 	}
 
 
